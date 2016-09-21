4049c519f0eb185b0beb5c9da4284ed453af6af4
chimaakp <chima@udel.edu>
2016-09-21 16:55:22 -0400

    Update lab2lottery.hpp
    kfyu

--- LabTwo/lab2lottery.hpp
+++ LabTwo/lab2lottery.hpp
@@ -2,14 +2,38 @@
  * lab2lottery.hpp
  *
  *  Created on: Sep 21, 2016
- *      Author: chima
+ *      Author: Ryan
  */
 
 #ifndef LAB2LOTTERY_HPP_
 #define LAB2LOTTERY_HPP_
 
 
-Struct Customer
+struct Customer{
+    int custId;
+    int numTickets;
+    int lotteryNums [numTickets][3];
+};
 
+struct Store{
+    int storeId = rand();
+    int numCust;
+    std::list<numCust> customerList;
+    int numSold;
+    int numWinners[3];
+};
+
+struct Owner{
+    Store *Stores;
+    int numStores = Stores/sizeof(int);
+    int totalSold;
+};
+
+Owner *makeOwner();
+Store makeStore();
+Customer makeCustomer ();
+void getWinners (int winners[]);
+void findWinners(Owner *owner, int *ls);
+int checkwin(int *nums, int *winners);
 
 #endif /* LAB2LOTTERY_HPP_ */
