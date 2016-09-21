19f403bcdc337e759e5b3f989e8cd9528f4d0161
chima <chima@128.4.93.120>
2016-09-21 16:26:28 -0400

    please

---
+++ LabTwo/lab2lottery.hpp
@@ -0,0 +1,15 @@
+/*
+ * lab2lottery.hpp
+ *
+ *  Created on: Sep 21, 2016
+ *      Author: chima
+ */
+
+#ifndef LAB2LOTTERY_HPP_
+#define LAB2LOTTERY_HPP_
+
+
+Struct Customer
+
+
+#endif /* LAB2LOTTERY_HPP_ */
